4256750479e66b1f2db87a8e1ea91dece519c51d
Thanh Tran <thanh.tran@livepayments.com>
2024-07-23 16:28:34 +0700

    Init

---
+++ eCommerce.NET.AppHost/Program.cs
@@ -0,0 +1,13 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var weatherApi = builder.AddProject<Projects.eCommerce_NET_MinimalApi>("weatherapi")
+    .WithExternalHttpEndpoints();
+
+builder.AddNpmApp("react", "../eCommerce.NET.React")
+    .WithReference(weatherApi)
+    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
+builder.Build().Run();
