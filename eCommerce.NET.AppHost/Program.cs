f510cec23e766897c1deed221d9112cf982ddc54
Thanh Tran <thanh.tran@livepayments.com>
2024-07-26 07:49:50 +0700

    Update names

--- eCommerce.NET.AppHost/Program.cs
+++ eCommerce.NET.AppHost/Program.cs
@@ -4,14 +4,14 @@ var redis = builder.AddRedis("redis");
 var rabbitMq = builder.AddRabbitMQ("eventbus");
 var postgres = builder.AddPostgres("postgres").WithImageTag("latest");
 
-var identityDb = postgres.AddDatabase("identitydb");
-var orderDb = postgres.AddDatabase("orderingdb");
+var identityDb = postgres.AddDatabase("identity-db");
+var orderDb = postgres.AddDatabase("ordering-db");
 
-var identityApi = builder.AddProject<Projects.eCommerce_NET_IdentityAPI>("identityapi")
+var identityApi = builder.AddProject<Projects.eCommerce_NET_IdentityAPI>("identity-api")
     .WithExternalHttpEndpoints()
     .WithReference(identityDb);
 
-var weatherApi = builder.AddProject<Projects.eCommerce_NET_MinimalApi>("weatherapi")
+var weatherApi = builder.AddProject<Projects.eCommerce_NET_MinimalApi>("weather-api")
     .WithExternalHttpEndpoints();
 
 var basketApi = builder.AddProject<Projects.eCommerce_NET_BasketAPI>("basket-api")
@@ -25,7 +25,7 @@ var api = builder.AddProject<Projects.eCommerce_NET_API>("api")
     .WithExternalHttpEndpoints();
 
 builder.AddNpmApp("react", "../eCommerce.NET.React")
-    .WithReference(weatherApi)
+    .WithReference(api)
     .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
